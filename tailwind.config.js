1d937d808f39c30589cb9cfa22fec1267b0f3518
최미소 <choemiso@choemisoui-MacBookPro.local>
2026-01-04 12:45:39 +0900

    fix: Correct Tailwind CSS configuration
    - Update postcss.config.js to correctly reference the tailwindcss plugin.
    
    - Use import instead of require in tailwind.config.js for consistency.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,5 +9,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 }
